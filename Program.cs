9819bbd48411ba725adf9347bbbd9fc6f68d79db
Lev Stipakov <lev@openvpn.net>
2025-03-13 10:45:58 +0200

    Remove installer code
    The service is installed via MSI Wix script.
    
    GitHub: #24
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- Program.cs
+++ Program.cs
@@ -11,57 +11,22 @@ namespace OpenVpn
     {
         public static int Main(string[] args)
         {
-            if (args.Length == 0)
+            if (!Environment.UserInteractive)
             {
-                if (!Environment.UserInteractive)
-                {
-                    // Running as a Windows Service
-                    ServiceBase.Run(new OpenVpnService());
-                }
-                else
-                {
-                    // Running as a console application
-                    Console.WriteLine("Running in console mode...");
-                    var runner = new OpenVPNServiceRunner(null);
-                    runner.Start(args);
-
-                    Console.WriteLine("Press Enter to stop...");
-                    Console.ReadLine();
-
-                    runner.Stop();
-                }
-            }
-            else if (args[0] == "-install")
-            {
-                try
-                {
-                    ProjectInstaller.Install();
-                }
-                catch (Exception e)
-                {
-                    Console.Error.WriteLine(e.Message);
-                    Console.Error.WriteLine(e.StackTrace);
-                    return 1;
-                }
-            }
-            else if (args[0] == "-remove")
-            {
-                try
-                {
-                    ProjectInstaller.Stop();
-                    ProjectInstaller.Uninstall();
-                }
-                catch (Exception e)
-                {
-                    Console.Error.WriteLine(e.Message);
-                    Console.Error.WriteLine(e.StackTrace);
-                    return 1;
-                }
+                // Running as a Windows Service
+                ServiceBase.Run(new OpenVpnService());
             }
             else
             {
-                Console.Error.WriteLine("Unknown command: " + args[0]);
-                return 1;
+                // Running as a console application
+                Console.WriteLine("Running in console mode...");
+                var runner = new OpenVPNServiceRunner(null);
+                runner.Start(args);
+
+                Console.WriteLine("Press Enter to stop...");
+                Console.ReadLine();
+
+                runner.Stop();
             }
             return 0;
         }
